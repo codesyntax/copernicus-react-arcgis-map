da1659aedcb0ed6fe3d253cc98c500cc9e44455d
bipoza <bipoza@gmail.com>
2021-05-12 16:52:25 +0200

    migrated jquery functions to vanilla JS

--- src/ArcgisMap/ArcgisMap.jsx
+++ src/ArcgisMap/ArcgisMap.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import BasemapGallery from "@arcgis/core/widgets/BasemapGallery";
@@ -9,14 +9,23 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import esriConfig from '@arcgis/core/config';
 import "./ArcgisMap.css";
 import "@arcgis/core/assets/esri/css/main.css";
-import MenuContainer from "./MenuContainer";
+// import MenuContainer from "./MenuContainer";
+// import classNames from 'classnames';
 
+let view;
 function ArcgisMap() {
-    const [showMapMenu, setShowMapMenu] = useState(false)
+    // const [showMapMenu, setShowMapMenu] = useState(false)
+    // const [view, setView] = useState()
+
     // esriConfig.assetsPath = '/assets';
     const mapDiv = useRef(null);
+    const menuContainerDiv = useRef(null);
+    // var menuClass = classNames('esri-widget--button esri-widget esri-interactive', {
+    //     'esri-icon-left-arrow': showMapMenu,
+    //     'esri-icon-drag-horizontal': !showMapMenu
+    // });
     useEffect(() => {
-        let view;
+
         if (mapDiv.current) {
             const map = new Map({
                 basemap: "topo-vector",
@@ -31,77 +40,83 @@ function ArcgisMap() {
                     components: ["attribution"] // removes default widgets except for attribution
                 }
             });
-            view.constraints = {
-                minZoom: 3
-            };
-            var zoom = new Zoom({
-                view: view
-            });
-            view.ui.add(zoom, {
-                position: "top-right"
-            });
-        };
-        var basemap_container = document.createElement('div');
-        basemap_container.className = "basemap-gallery-container";
-        var basemap_button = document.createElement('div');
-        basemap_button.className = "esri-icon-basemap esri-widget--button esri-widget esri-interactive";
-        basemap_button.id = "map_basemap_button";
-        basemap_button.setAttribute("role", "button");
-        basemap_button.setAttribute("title", "Basemap gallery");
-        // basemap_button.addEventListener('click', function(evt){
-        //   if (this.classList.contains("esri-icon-basemap")) {
-        //     $(".esri-basemap-gallery").appendTo(".basemap-gallery-container").show();
-        //     $(this).removeClass("esri-icon-basemap").addClass("esri-icon-right-arrow")
-        //   }
-        //   else if (this.classList.contains("esri-icon-right-arrow")) {
-        //     $(".esri-basemap-gallery").hide();
-        //     $(this).removeClass("esri-icon-right-arrow").addClass("esri-icon-basemap")
-        //   }
-        // });
-        basemap_container.appendChild(basemap_button);
-        view.ui.add(basemap_container, "top-right");
+            console.log(view)
+            if (view) {
 
-        const basemapGallery = new BasemapGallery({
-            view: view,
-        });
-        view.ui.add(basemapGallery, "top-right");
+                view.constraints = {
+                    minZoom: 3
+                };
+                var zoom = new Zoom({
+                    view: view
+                });
+                view.ui.add(zoom, {
+                    position: "top-right"
+                });
 
-        // Print
-        var print_button = document.createElement('div');
-        print_button.className = "esri-icon-printer esri-widget--button esri-widget esri-interactive";
-        print_button.setAttribute("role", "button");
-        print_button.setAttribute("title", "Print");
-        print_button.addEventListener('click', function (evt) {
-            // action
-        });
-        view.ui.add(print_button, "top-right");
+                var basemap_container = document.createElement('div');
+                basemap_container.className = "basemap-gallery-container";
+                var basemap_button = document.createElement('div');
+                basemap_button.className = "esri-icon-basemap esri-widget--button esri-widget esri-interactive";
+                basemap_button.id = "map_basemap_button";
+                basemap_button.setAttribute("role", "button");
+                basemap_button.setAttribute("title", "Basemap gallery");
+                basemap_button.addEventListener('click', function(evt){
+                  if (this.classList.contains("esri-icon-basemap")) {
+                    document.getElementsByClassName("esri-basemap-gallery")[0].classList.add("basemap-gallery-container")
+                    document.getElementsByClassName("esri-basemap-gallery")[0].style.display = 'block';
+                    this.classList.remove("esri-icon-basemap");
+                    this.classList.add("esri-icon-right-arrow");
+                  }
+                  else if (this.classList.contains("esri-icon-right-arrow")) {
+                    document.getElementsByClassName("esri-basemap-gallery")[0].style.display = 'none';
+                    this.classList.remove("esri-icon-right-arrow");
+                    this.classList.add("esri-icon-basemap");
+                  }
+                });
+                basemap_container.appendChild(basemap_button);
+                view.ui.add(basemap_container, "top-right");
 
-        // Area selection
-        var area_button = document.createElement('div');
-        area_button.className = "esri-icon-cursor-marquee esri-widget--button esri-widget esri-interactive";
-        area_button.setAttribute("role", "button");
-        area_button.setAttribute("title", "Select area");
-        area_button.addEventListener('click', function (evt) {
-            // action
-        });
-        view.ui.add(area_button, "top-right");
+                const basemapGallery = new BasemapGallery({
+                    view: view,
+                });
+                view.ui.add(basemapGallery, "top-right");
 
-        // Legend
-        var legend_button = document.createElement('div');
-        legend_button.className = "esri-icon-legend esri-widget--button esri-widget esri-interactive";
-        legend_button.setAttribute("role", "button");
-        legend_button.setAttribute("title", "Legend");
-        legend_button.addEventListener('click', function (evt) {
-            // action
-        });
-        view.ui.add(legend_button, "top-right");
+                // Print
+                var print_button = document.createElement('div');
+                print_button.className = "esri-icon-printer esri-widget--button esri-widget esri-interactive";
+                print_button.setAttribute("role", "button");
+                print_button.setAttribute("title", "Print");
+                print_button.addEventListener('click', function (evt) {
+                    // action
+                });
+                view.ui.add(print_button, "top-right");
 
-        // Menu
-        var menu_container = document.createElement('div');
-        menu_container.className = "map-left-menu-container"
-        var menu_button = document.createElement('div');
-        menu_button.className = "esri-icon-left-arrow esri-widget--button esri-widget esri-interactive";
-        menu_container.innerHTML = `
+                // Area selection
+                var area_button = document.createElement('div');
+                area_button.className = "esri-icon-cursor-marquee esri-widget--button esri-widget esri-interactive";
+                area_button.setAttribute("role", "button");
+                area_button.setAttribute("title", "Select area");
+                area_button.addEventListener('click', function (evt) {
+                    // action
+                });
+                view.ui.add(area_button, "top-right");
+
+                // Legend
+                var legend_button = document.createElement('div');
+                legend_button.className = "esri-icon-legend esri-widget--button esri-widget esri-interactive";
+                legend_button.setAttribute("role", "button");
+                legend_button.setAttribute("title", "Legend");
+                legend_button.addEventListener('click', function (evt) {
+                    // action
+                });
+                view.ui.add(legend_button, "top-right");
+
+                // Menu
+                var menu_container = document.createElement('div');
+                menu_container.className = "map-left-menu-container"
+                var menu_button = document.createElement('div');
+                menu_button.className = "esri-icon-drag-horizontal esri-widget--button esri-widget esri-interactive";
+                menu_container.innerHTML = `
                 <div class="map-menu tab-container">
                     <div class="tabs" role="tablist">
                         <span class="tab tab-selected" id="products_label" role="tab" aria-controls="products_panel" aria-selected="true">Products and datasets</span>
@@ -112,43 +127,47 @@ function ArcgisMap() {
                         <div class="panel" id="active_panel" role="tabpanel" aria-hidden="true"><div class="no-active-datasets">No datasets selected</div></div>
                     </div>
                 </div>`;
-        menu_button.addEventListener('click', function (evt) {
-            console.log(evt)
-            if (this.classList.contains("esri-icon-drag-horizontal")) {
-                setShowMapMenu(true)
-                // $(".map-menu").show();
-                // $(this).removeClass("esri-icon-drag-horizontal").addClass("esri-icon-left-arrow")
-            }
-            else if (this.classList.contains("esri-icon-left-arrow")) {
-                // $(".map-menu").hide();
-                // $(this).removeClass("esri-icon-left-arrow").addClass("esri-icon-drag-horizontal")
-            }
-        });
-        menu_container.appendChild(menu_button);
-        view.ui.add(menu_container, "top-left");
-        // loadTestProducts();
+                menu_button.addEventListener('click', function (evt) {
+                    console.log(evt)
+                    if (this.classList.contains("esri-icon-drag-horizontal")) {
+                        document.getElementsByClassName("map-menu")[0].style.display = 'block';
+                        this.classList.remove("esri-icon-drag-horizontal");
+                        this.classList.add("esri-icon-left-arrow");
+                    }
+                    else if (this.classList.contains("esri-icon-left-arrow")) {
+                        document.getElementsByClassName("map-menu")[0].style.display = 'none';
+                        this.classList.remove("esri-icon-left-arrow");
+                        this.classList.add("esri-icon-drag-horizontal");
+                    }
+                });
+                menu_container.appendChild(menu_button);
+                view.ui.add(menu_container, "top-left");
 
-        // Scalebar
-        var scaleBar = new ScaleBar({
-            view: view,
-            unit: "dual"
-        });
-        view.ui.add(scaleBar, "bottom-left");
 
-        // Loading icon
-        view.watch('updating', function (evt) {
-            if (evt === true) {
-                document.querySelector(".loading").style.display = 'flex';
-            } else {
-                document.querySelector(".loading").style.display = 'none';
+                // loadTestProducts();
+
+                // Scalebar
+                var scaleBar = new ScaleBar({
+                    view: view,
+                    unit: "dual"
+                });
+                view.ui.add(scaleBar, "bottom-left");
+
+                // Loading icon
+                view.watch('updating', function (evt) {
+                    if (evt === true) {
+                        document.querySelector(".loading").style.display = 'flex';
+                    } else {
+                        document.querySelector(".loading").style.display = 'none';
+                    }
+                })
             }
-        })
+        };
     }, []);
+
     return (
         <>
-                <MenuContainer></MenuContainer>
             <div className="map-container">
-                
                 <div ref={mapDiv} className="map">
                 </div>
                 <div className="loading"> <FontAwesomeIcon icon={faSpinner} pulse /></div>
--- src/ArcgisMap/MenuContainer.jsx
+++ src/ArcgisMap/MenuContainer.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import "@arcgis/core/assets/esri/css/main.css";
 import "./ArcgisMap.css";
+import classNames from 'classnames';
 
-function MenuContainer() {
+
+const MenuContainer = React.forwardRef((props, ref) => {
+    // const FancyButton = React.forwardRef((props, ref) => (
+    //     <button ref={ref} className="FancyButton">
+    //       {props.children}
+    //     </button>
+    //   ));
+      
     const [showMapMenu, setShowMapMenu] = useState(false)
-
+    var menuClass = classNames('esri-widget--button esri-widget esri-interactive', {
+        'esri-icon-left-arrow':showMapMenu,
+        'esri-icon-drag-horizontal': !showMapMenu
+      });
     // var menu_container = document.createElement('div');
     // menu_container.className = "map-left-menu-container"
     // var menu_button = document.createElement('div');
@@ -34,32 +45,28 @@ function MenuContainer() {
     // });
     // menu_container.appendChild(menu_button);
 
-    function toggleMenu(){
-        if(showMapMenu){
-            setShowMapMenu(false);
-        }else{
-            setShowMapMenu(true);
-
-        }
-    }
+function aaa(){
+    setShowMapMenu(!showMapMenu);
+    console.log("CLICK")
+}
     return (<>
-        <div className="esri-icon-drag-horizontal esri-widget--button esri-widget esri-interactive" onClick={() => toggleMenu()}></div>
+        <div className={menuClass} onClick={() => aaa()} ref={ref}>
         {showMapMenu ?
             <div className="map-left-menu-container">
-                KSKJNSKHNSL
-                <div class="map-menu tab-container">
-                    <div class="tabs" role="tablist">
-                        <span class="tab tab-selected" id="products_label" role="tab" aria-controls="products_panel" aria-selected="true">Products and datasets</span>
-                        <span class="tab" id="active_label" role="tab" aria-controls="active_panel" aria-selected="false">Active on map</span>
+                <div className="map-menu tab-container">
+                    <div className="tabs" role="tablist">
+                        <span className="tab tab-selected" id="products_label" role="tab" aria-controls="products_panel" aria-selected="true">Products and datasets</span>
+                        <span className="tab" id="active_label" role="tab" aria-controls="active_panel" aria-selected="false">Active on map</span>
                     </div>
-                    <div class="panels">
-                        <div class="panel panel-selected" id="products_panel" role="tabpanel" aria-hidden="false"></div>
-                        <div class="panel" id="active_panel" role="tabpanel" aria-hidden="true"><div class="no-active-datasets">No datasets selected</div></div>
+                    <div className="panels">
+                        <div className="panel panel-selected" id="products_panel" role="tabpanel" aria-hidden="false"></div>
+                        <div className="panel" id="active_panel" role="tabpanel" aria-hidden="true"><div className="no-active-datasets">No datasets selected</div></div>
                     </div>
                 </div>
             </div>:''
         }
+        </div>
     </>);
-}
+});
 
 export default MenuContainer;
